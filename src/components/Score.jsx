883d54d73747b718210081dd13c7c4adae41eba2
tomasgentile <ltomass@hotmail.com>
2023-04-10 21:06:26 -0300

    score container

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -18,10 +18,9 @@ const Score = () => {
 
     return (
         <div className={styles.scoreContainer}>
-            <div>{scoreRemoteGame[0] ? getIcon(scoreRemoteGame[0]) : defaultIcon}</div>
-            <div>{scoreRemoteGame[1] ? getIcon(scoreRemoteGame[1]) : defaultIcon}</div>
-            <div>{scoreRemoteGame[2] ? getIcon(scoreRemoteGame[2]) : defaultIcon}</div>
-
+            <div className={styles.individualScoreContaner}>{scoreRemoteGame[0] ? getIcon(scoreRemoteGame[0]) : defaultIcon}</div>
+            <div className={styles.individualScoreContaner}>{scoreRemoteGame[1] ? getIcon(scoreRemoteGame[1]) : defaultIcon}</div>
+            <div className={styles.individualScoreContaner}>{scoreRemoteGame[2] ? getIcon(scoreRemoteGame[2]) : defaultIcon}</div>
         </div>
     )
 }
